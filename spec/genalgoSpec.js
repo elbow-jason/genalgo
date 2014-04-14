30c502fb2318f894780dd45d5423d8da0710460a
jlgoldb2 <jlgoldb2@asu.edu>
2014-04-14 12:13:31 -0700

    genBasesObj is passing tests

--- spec/genalgoSpec.js
+++ spec/genalgoSpec.js
@@ -21,10 +21,10 @@ describe(".ranAddr", function() {
   it("returns a random index from a given sequence or array" , function() {
 
     spyOn(Math, "random").and.returnValue(0.1);
-    
-    expect(Helpers.randIndex("AAAA")).toBe(0);
-    expect(Helpers.randIndex([0,0,0])).toBe(0);
-    expect(Helpers.randIndex([1])).toBe(0);
+
+    expect(helpers.randIndex("AAAA")).toBe(0);
+    expect(helpers.randIndex([0,0,0])).toBe(0);
+    expect(helpers.randIndex([1])).toBe(0);
   });
 });
 
@@ -32,12 +32,12 @@ describe(".torf", function() {
 
   it("returns true or false at random (this test is for true)" , function() {
     spyOn(Math, "random").and.returnValue(0.3);
-    expect(Helpers.torf()).toBe(true);
+    expect(helpers.torf()).toBe(true);
   });
 
   it("returns true or false at random (this test is for true)" , function() {
     spyOn(Math, "random").and.returnValue(0.7);
-    expect($gene.torf()).toBe(false);
+    expect(helpers.torf()).toBe(false);
   });
 });
 
@@ -45,7 +45,7 @@ describe(".torf", function() {
 describe(".genBasesObj", function() {
 
   it("returns an object to decode sequences into functions (ribosome)", function() {
-    expect($gene.genBasesObj("AB",["func1", "func2"])).toEqual({"A" : "func1", "B" : "func2"});
+    expect(helpers.genBasesObj("AB",["func1", "func2"])).toEqual({"A" : "func1", "B" : "func2"});
   });
 });
 
@@ -53,7 +53,7 @@ describe(".genBasesObj", function() {
 describe(".randomSeq", function(){
 
   it("returns a random sequence given a desired length and a codex of available characters", function(){
-    expect($gene.randomSeq(4, "A")).toBe("AAAA");
+    expect(helpers.randomSeq(4, "A")).toBe("AAAA");
   });
 });
 
@@ -61,15 +61,15 @@ describe(".randReverse", function() {
 
   it("returns a given sequence in reverse or forward, randomly (0.9)", function() {
     spyOn(Math, "random").and.returnValue(0.9);
-    expect($gene.randReverse("ABCD")).toBe("ABCD");
-    expect($gene.randReverse("XYZ")).toBe("XYZ");
+    expect(helpers.randReverse("ABCD")).toBe("ABCD");
+    expect(helpers.randReverse("XYZ")).toBe("XYZ");
   });
 
 
   it("returns a given sequence in reverse or forward, randomly (0.9)", function() {
     spyOn(Math, "random").and.returnValue(0.3);
-    expect($gene.randReverse("ABCD")).toBe("DCBA");
-    expect($gene.randReverse("XYZ")).toBe("ZYX");
+    expect(helpers.randReverse("ABCD")).toBe("DCBA");
+    expect(helpers.randReverse("XYZ")).toBe("ZYX");
   });
 });
 
@@ -78,11 +78,11 @@ describe("Organism.addChr", function() {
   var rabbit;
 
   beforeEach(function() {
-    rabbit = new $gene.Organism('AASSDDFF');
+    rabbit = new helpers.Organism('AASSDDFF');
     rabbit.addChr('FFSSDDAA');
   });
 
-  it("adds sequences to $gene.Organism.chromosomes", function() {
+  it("adds sequences to helpers.Organism.chromosomes", function() {
     expect(rabbit.chromosomes[0]).toBe('AASSDDFF');
     expect(rabbit.chromosomes[1]).toBe('FFSSDDAA');
   });
@@ -92,7 +92,7 @@ describe("Organism.addChr", function() {
 
 describe("Mutate.point", function() {
 
-  var mut = new $gene.Mutate();
+  var mut = new helpers.Mutate();
 
   it("given a symbol array and sequence, it causes a point mutation", function() {
     spyOn(Math, "random").and.returnValue(0.01);
@@ -106,7 +106,7 @@ describe("Mutate.point", function() {
 
 describe("Mutate.insert", function() {
 
-  var mut = new $gene.Mutate();
+  var mut = new helpers.Mutate();
 
   it("returns a random insertion of fragment of given sequence (seq + fragment + uence)", function() {
     expect(mut.insert("AA")).toMatch(/^[A]{2,4}$/);
@@ -125,7 +125,7 @@ describe("Mutate.insert", function() {
 
 describe("Mutate.sliceSeq", function() {
 
-  var mut = new $gene.Mutate();
+  var mut = new helpers.Mutate();
   it("returns insertion of given fragment into given sequence at random index of sequence", function() {
     spyOn(Math, "random").and.returnValue(0.3);
     expect(mut.sliceSeq("AAAAAAAAA", "BBB")).toBe("AABBBAAAAAAA");
@@ -135,7 +135,7 @@ describe("Mutate.sliceSeq", function() {
 
 describe("Mutate.fragment", function() {
 
-  var mut = new $gene.Mutate();
+  var mut = new helpers.Mutate();
   it("returns a random fragment, in reverse or forward, from a given sequence", function() {
     expect(mut.fragment("QQ")).toMatch(/^[Q]{1,2}$/);
   });
@@ -151,7 +151,7 @@ describe("Mutate.fragment", function() {
 
 describe("Mutate.transpose", function() {
 
-  var mut = new $gene.Mutate();
+  var mut = new helpers.Mutate();
   it("given two sequences in an array [seq1, seq2], it will trade endcaps on the sequences and return an array", function() {
     spyOn(Math, "random").and.returnValue(0.3);
     expect(mut.transpose(["QQ","AA"])).toBe(/^[Q]{1,2}$/);
